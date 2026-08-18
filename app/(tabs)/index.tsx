86c846bc00e642af787f84820fb9a3667f3e3128
Krishna <muralikrishna.bokki@hcl-software.com>
2026-08-18 22:43:39 +0530

    feat: tab navigation, custom tabbar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreView);
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
@@ -41,6 +44,6 @@ export default function App() {
       >
         Netflix Subscription
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
